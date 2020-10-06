40a8d869ec9cf06abec09add5aafc079039ca7e2
nullptr <2281525307@qq.com>
2020-10-06 20:15:40 +0800

    项目初始化

---
+++ Log/Log.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+#include "Log.h"
+#include "CommonUtil.h"
+
+#include <fstream>
+
+using namespace std;
+using namespace algorithm;
+
+string Log::logFileName = getNowDateStr() + string(".log");
+
+Log::Log(const std::string & level, const std::string & file,
+	const int & line, const std::string & methodName, const char * format, ...)
+	: Log(level, file, line, methodName, string())
+{
+	va_list args;
+	char msg[4096];
+	va_start(args, format);
+	vsnprintf(msg, 4096, format, args);
+	va_end(args);
+	this->msg = msg;
+}
+
+Log::Log(const string & level, const std::string& file,const int& line, 
+	const std::string& methodName, const std::string& msg)
+	: level(level), line(line), methodName(methodName), msg(msg)
+{
+	this->file = getRelativePath(file);
+	this->time = getNowTimeStr();
+}
+
+void Log::writeToFile()
+{
+	ofstream stream(logFileName, ios::app);
+
+	stream << this->time << " ";
+	stream << "[" << this->level << "] ";
+	stream << "(" << this->file << ":" << this->line << ") ";
+	stream << "<" << this->methodName << "> ";
+	stream << this->msg << endl;
+
+	stream.close();
+}
---
+++ Log/Log.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "pch.h"
+#include <string>
+
+#define ERROR_LOG(format, ...) Log("ERROR", __FILE__, __LINE__,  __func__, format, __VA_ARGS__).writeToFile()
+#define WARN_LOG(format, ...) Log("WARN", __FILE__, __LINE__,  __func__, format, __VA_ARGS__).writeToFile()
+#define INFO_LOG(format, ...) Log("INFO", __FILE__, __LINE__,  __func__, format, __VA_ARGS__).writeToFile()
+#define DEBUG_LOG(format, ...) Log("DEBUG", __FILE__, __LINE__,  __func__, format, __VA_ARGS__).writeToFile()
+#define LOG(level, msg) Log(level, __FILE__, __LINE__,  __func__, msg).writeToFile()
+
+class Log  final
+	{
+	public:
+		_declspec(dllexport) Log(const std::string& level, const std::string& file, const int& line, const std::string& methodName, const char* format, ...);
+		_declspec(dllexport) Log(const std::string& level, const std::string& file, const int& line, const std::string& methodName, const std::string& msg);
+
+	public:
+		_declspec(dllexport) void writeToFile();
+
+	private:
+		std::string time;
+		std::string level;
+		std::string file;
+		int line;
+		std::string methodName;
+		std::string msg;
+
+	private:
+		static std::string logFileName;
+};
